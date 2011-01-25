4ca512ab4998886bcdc765f849f3814ad1ec2c8d
A. Bram Neijt <bneijt@gmail.com>
2011-01-25 10:37:41 +0100

    Implement meanIntensity
    Looked at all the different Java image
    manipulation libraries, could not find
    one that looked pleasing enough to include.
    
    Ended up doing the coding myself, probably
    not the best implementation, but it should
    work.

--- src/main/java/nl/bneijt/videosaic/MeanIdentProducer.java
+++ src/main/java/nl/bneijt/videosaic/MeanIdentProducer.java
@@ -13,11 +13,42 @@ class MeanIdentProducer implements IdentProducer
     */
     public String identify(BufferedImage i)
     {
-        //Split the image into 4 quadrants
+        //Split the image into 4 quadrants [a b; c d]
+    	int w = i.getWidth() / 2;
+    	int h = i.getHeight() / 2;
+    	BufferedImage a = i.getSubimage(0, 0, w, h);
+    	BufferedImage b = i.getSubimage(w, 0, w, h);
+    	BufferedImage c = i.getSubimage(0, h, w, h);
+    	BufferedImage d = i.getSubimage(w, h, w, h);
+    	
         //Find the mean intensity of each quadrant
-        char levels[] = {254,0,18,2}; //Resulting intensities
-        //Return the value as a string
-        return "";//(new String(".")).join(levels);
+    	int[] levels = {meanIntensity(a), meanIntensity(b), meanIntensity(c), meanIntensity(d)};
+        
+    	//Return the value as a string
+        return levels[0] + "." + levels[1] + "." + levels[2] + "." + levels[3];
+    }
+    /**
+     * Return the mean intensity of the BufferedImage i using summed RGB value of all pixels
+     * @param i
+     * @return
+     */
+    public int meanIntensity(BufferedImage i)
+    {
+    	long sum = 0;
+    	final int w = i.getWidth();
+    	final int h = i.getHeight();
+    	for(int x = 0; x < w; ++x)
+    		for(int y = 0; y < h; ++y)
+    		{
+    			int pixel = i.getRGB(x, y);
+    			//int alpha = (pixel >> 24) & 0xff;
+    		    int red = (pixel >> 16) & 0xff;
+    		    int green = (pixel >> 8) & 0xff;
+    		    int blue = (pixel) & 0xff;
+
+    			sum += red + green + blue;
+    		}
+    	return (int)(sum / (3 * w * h)); 
     }
 }
 
